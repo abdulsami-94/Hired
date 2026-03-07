e65998a1adfb5a872e2f7f4e5d531951a3427852
Abdul Sami Shaikh <abdulsamishaikh232@gmail.com>
2026-03-07 13:10:16 +0530

    Added Routes for testing

---
+++ src/app/api/health/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+// This is a GET request handler
+export async function GET() {
+    return NextResponse.json(
+        {
+            status: "alive",
+            message: "The backend is breathing ... for now.",
+            timestamp: new Date().toISOString()
+        },
+        {
+            status:200
+        }
+    );
+}
